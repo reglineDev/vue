3908d79dc1b11bb7102f6f839807a6cb05b3a709
regline <regline@xrp.kr>
2022-05-24 13:48:38 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,123 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div class="black-bg" v-if="modalOpen == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="modalOpen=false">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="(a, i) in menu" :key="i">{{ a }} {{ i }}</a>
+  </div>
+
+
+  <div v-for="(a, i) in products" :key="a">
+    <h4>{{ products[i] }}</h4>
+    <p>XX 만원</p>
+    <button @click="신고Count[i]++">허위매물신고</button>
+    <span>신고수 : {{ 신고Count[i] }}</span>
+  </div>
+
+  <div>
+    <p>-----------------------------------------------</p>
+  </div>
+
+  <div>
+    <img :src="oneRooms[0].image" class="room-img">
+    <h4 @click="modalOpen = true">{{ oneRooms[0].title }}</h4>
+    <p> {{ oneRooms[0].price }} 만원</p>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[0] }}</span>
+  </div>
+  <div>
+    <img src="./assets/room02.jpg" class="room-img">
+    <h4>{{ products[1] }}</h4>
+    <p>50 만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[1] }}</span>
+  </div>
+  <div>
+    <img src="./assets/room03.jpg" class="room-img">
+    <h4>{{ products[2] }}</h4>
+    <p>50 만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[2] }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import oneRoomData from './assets/oneroom.js';
+
+  export default {
+    name: 'App',
+    data(){
+      return{
+        oneRooms : oneRoomData,
+        modalOpen: false,
+        신고Count : [0, 0, 0],
+        신고수 : [0, 0, 0],
+        menu : ['Home', 'Shop', 'About'],
+        products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+      }
+    },
+    methods :{
+      increase(arrNum){
+        this.신고수[arrNum] += 1;
+      }
+    },
+
+    component:{
+
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  body{
+    margin: 0;
+  }
+
+  div{
+    box-sizing: border-box;
+  }
+
+  .black-bg{
+    width: 100%; height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed; padding:20px;
+  }
+
+  .white-bg{
+    width: 100%; background: white;
+    border-radius: 8px;
+    padding: 20px;
+  }
+
+  .room-img{
+    width: 300px;
+    margin-top: 40px;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .menu{
+    background: darkslateblue;
+    padding: 15px;
+    border-radius: 5px;
+  }
+  .menu a{
+    color: white;
+    padding: 10px;
+  }
+
 </style>
---
+++ src/assets/oneroom.js
@@ -0,0 +1,17 @@
+export default 
+[
+    {
+        id: 0,
+        title: "첫번째 원룸",
+        image: "http://codingapple1.github.io/vue/room0.jpg",
+        content: "22년 신축 공사한 남향 원룸 ",
+        price: 340000
+    },
+    {
+        id: 1,
+        title: "두번째 원룸",
+        image: "http://codingapple1.github.io/vue/room0.jpg",
+        content: "33년 신축 공사한 남향 원룸 ",
+        price: 560000
+    }
+]
